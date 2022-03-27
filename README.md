70e3773598c25c08bb2bcd5fc892d5325b8e08cf
Raleigh Littles III <raleighlittles@gmail.com>
2022-03-26 17:08:00 -0700

    Ready to publish

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Olympus-MAJ-1428-driver
-A driver for the Olympus MAJ-1428 medical keyboard
+# About
+
+This is a Linux driver for the Olympus MAJ-1428 medical keyboard.
+
+![keyboard-stock-image](./docs/olympus-maj1428.jpg)
+
+The source code has been intentionally over-annotated for educational purposes.
+
+# Context
+
+For whatever reason, the 5.13 kernel's HID driver is missing support for 3 of the 9 special keys on the top of the keyboard (in the purple area). I wanted to write a driver that would fix that, and also map the rest of the special keys to function keys instead of normal characters.
+
+# Setup
+
+# Install kernel headers
+
+Before you start, you'll need to install the header files for your kernel.
+
+Run: 
+
+```bash
+$ sudo apt install linux-headers-$(uname -r)
+```
+
+# Compile the driver
+
+Next, run `make` in the root directory of this repository.
+
+This will compile your kernel module (.ko file).
+
+
+You might get a warning that says:
+
+`Skipping BTF generation for hid-olympus-maj1428.ko due to unavailability of vmlinux`
+
+That's okay.
+
+# Load the kernel module
+
+Run:
+
+```bash
+$ sudo insmod hid-olympus-maj1428.ko 
+```
+
+Now you can use the device as needed.
+
+If you want to go back to using the built-in kernel driver instead, you can remove this kernel module:
+
+
+```bash
+$ sudo rmmod hid-olympus-maj1428.ko 
+```
--- hid-olympus-maj1428.c
+++ hid-olympus-maj1428.c
@@ -4,16 +4,17 @@
 #include <linux/module.h>
 #include <linux/hid.h>
 
-//#include "hid-ids.h"
+// This header is only accessible when you're compiling against the kernel itself, but isn't available inside the usual kernel headers.
+//#include "hid-ids.h" 
 
 MODULE_AUTHOR("Raleigh Littles <raleighlittles@gmail.com>");
 MODULE_DESCRIPTION("Olympus MAJ 1428 driver");
 MODULE_LICENSE("GPL");
 
-/* # Description
+/* 
 
 This keyboard actually has 3 different USB devices included in it. 
-1. On the top right, there's a joystick-like controller (treated as a mouse), and a button [this is one device]
+1. On the top right, there's a joystick-like controller (treated as a mouse), and a button (emulating a mouse click)
 2. Starting at the top left, there are 9 keys:
 - '#PER PAGE'
 - 'CAPTURE
@@ -21,14 +22,43 @@ This keyboard actually has 3 different USB devices included in it.
 - 'PRINT'
 - 'PRINT QTY.'
 - 'COLOR'
-(This is referred to as the 'special keys')
+- 'FREEZE'
+- 'RELEASE'
+- 'EXAM END'
+(The keys listed above are  referred to as the 'special keys')
 3. Lastly, the main keyboard area (standard 87 keys) are treated as their own USB device
 
-Each of the 3 devices has a different USB product ID.
+Each of the 3 devices has a different USB product ID
+
 */
-#define USB_DEVICE_ID_OLYMPUS_MAJ1428_SPECIAL_KEYS 0x009b
+
+#define USB_PRODUCT_ID_OLYMPUS_MAJ1428_SPECIAL_KEYS 0x009b
 #define USB_VENDOR_ID_SUN 0x0430 // this is normally available in hid-ids.h, I just added it here for local debugging
 
+// Can't use "#" in a macro name, so use the symbol name instead
+#define OLYMPUS_MAJ1428_KEY_NUMBERSIGN_PER_PAGE 0xA6  // 166d
+#define OLYMPUS_MAJ1428_KEY_CAPTURE 0xA4   // 164d
+#define OLYMPUS_MAJ1428_KEY_DEL_IMAGE 0xA7 // 167d
+#define OLYMPUS_MAJ1428_KEY_PRINT 0xA8     // 168d
+#define OLYMPUS_MAJ1428_KEY_PRINT_QTY 0xA9 // 169d
+#define OLYMPUS_MAJ1428_KEY_COLOR 0xAD     // 173d
+#define OLYMPUS_MAJ1428_KEY_FREEZE 0xAA    // 170d
+#define OLYMPUS_MAJ1428_KEY_RELEASE 0xAB   // 171d
+#define OLYMPUS_MAJ1428_KEY_EXAM_END 0xAC  // 172d
+
+/// Keyboard mapping for the device.
+static unsigned char olympus_maj1428_key_mapping[][2] = {
+	{ OLYMPUS_MAJ1428_KEY_NUMBERSIGN_PER_PAGE, KEY_F13 },
+	{ OLYMPUS_MAJ1428_KEY_CAPTURE, KEY_F14 },
+	{ OLYMPUS_MAJ1428_KEY_DEL_IMAGE, KEY_F15},
+	{ OLYMPUS_MAJ1428_KEY_PRINT, KEY_F16},
+	{ OLYMPUS_MAJ1428_KEY_PRINT_QTY, KEY_F17},
+	{ OLYMPUS_MAJ1428_KEY_COLOR, KEY_F18},
+	{ OLYMPUS_MAJ1428_KEY_FREEZE, KEY_F19},
+	{ OLYMPUS_MAJ1428_KEY_RELEASE, KEY_F20},
+	{ OLYMPUS_MAJ1428_KEY_EXAM_END, KEY_F21},
+	{ 0, 0 } // Last element must be empty
+};
 
 /**
  * @brief Receives and interprets raw HID data from the keyboard, and then generates the appropriate key press event.
@@ -36,13 +66,14 @@ Each of the 3 devices has a different USB product ID.
  * @param data - A pointer to the start of an 8-byte array. This 8 byte array contains the HID data. This matches what you'll
  *               see in the URB_INTERRUPT packets from the keyboard on wireshark.
  */
-static int olympus_maj1428_raw_event(struct hid_device* hdev, struct hid_report*, u8* data, int size) {
+static int olympus_maj1428_raw_event(struct hid_device* hdev, struct hid_report*, u8* data, int /* size */) {
 
 	// The 3rd byte in the array contains the actual keyboard scan code, this is what we need to identify which key was pressed.
 	// Look up the HID packet interface for details
 	u8 key_scancode = data[2];
 
-    dev_printk(KERN_INFO, &hdev->dev, "key event detected! scan code = %d", data[2]);
+	// Debug only
+    // dev_printk(KERN_INFO, &hdev->dev, "key event detected! scan code = %d", data[2]);
 
 	// You'll need to get a pointer to the actual input_dev object to be able to produce a key event from it
 	struct input_dev* input;
@@ -50,18 +81,11 @@ static int olympus_maj1428_raw_event(struct hid_device* hdev, struct hid_report*
 	hidinput = list_entry(hdev->inputs.next, struct hid_input, list);
 	input = hidinput->input;
 
-	switch (key_scancode) {
-		// See the keymap in the README for more information
-		case 166: // 0xA6
-			input_report_key(input, KEY_F13, 1);
-			input_report_key(input, KEY_F13, 0);
-			break;
-
-		case 164:
-			input_report_key(input, KEY_F14, 1);
-			input_report_key(input, KEY_F14, 0);
-			break;
-	}
+	
+	// Pressing a key on a keyboard actually creates two events, an event that the key was pressed (when your finger pushes down on it),
+	// and then another event when they key is released (when your finger goes back up) -- hence the 1 and 0
+	input_report_key(input, olympus_maj1428_key_mapping[key_scancode][1], 1);
+	input_report_key(input, olympus_maj1428_key_mapping[key_scancode][1], 0);
 
 	return 0;
 }
@@ -74,7 +98,7 @@ static void olympus_maj1428_remove(struct hid_device* hdev) {
 /// Device strings
 static const struct hid_device_id olympus_maj1428_id_table[] = {
 	{ HID_USB_DEVICE(USB_VENDOR_ID_SUN,
-			 USB_DEVICE_ID_OLYMPUS_MAJ1428_SPECIAL_KEYS) },
+			 USB_PRODUCT_ID_OLYMPUS_MAJ1428_SPECIAL_KEYS) },
 	{}
 };
 MODULE_DEVICE_TABLE(hid, olympus_maj1428_id_table);
@@ -84,10 +108,10 @@ static struct hid_driver olympus_maj1428_driver = {
 	.id_table		= olympus_maj1428_id_table,
 /// This component is only for tweaking the report descriptor on events. We don't need to do this and can just use the scancodes
 /// from the device directly
-//  .report_fixup		= nullptr,
+//  .report_fixup		= 
 /// This callback runs after the kernel has positively identified our device. You can use this to check if the device is working
 /// by sending out a sample command, etc, but we don't need it here.
-//	.probe			= cougar_probe,
+//	.probe			= 
 /// This callback runs after the device has been removed
 	.remove			= olympus_maj1428_remove,
 /// This callback is the event handler for a keyboard event. 
